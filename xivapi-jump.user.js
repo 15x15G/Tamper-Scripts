c9c6b459bcf65457735bfb38a617afaf82445310
15x15 <65473527+15x15G@users.noreply.github.com>
2022-02-08 14:04:56 +0800

    Create xivapi-jump.user.js

---
+++ xivapi-jump.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         jump
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       15x15
+// @match        *://*.cafemaker.wakingsands.com/*
+// @match        *://*.xivapi.com/*
+// @icon         https://cafemaker.wakingsands.com/favicon.ico
+// @grant             GM_registerMenuCommand
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    function registerMenuCommand()
+    {
+        GM_registerMenuCommand('上一页', () =>
+                               {
+            const array = window.location.href.match(/.*\/(\w+)/)
+            if (array.length < 2) return;
+            const num = Number.parseInt(array[1])
+            window.location.href = window.location.href.replace(num, num - 1)
+        });
+        GM_registerMenuCommand('下一页', () =>
+                               {
+            const array = window.location.href.match(/.*\/(\w+)/)
+            if (array.length < 2) return;
+            const num = Number.parseInt(array[1])
+            window.location.href = window.location.href.replace(num, num + 1)
+        });
+    }
+    registerMenuCommand();
+})();
