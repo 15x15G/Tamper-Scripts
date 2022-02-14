5cb6d81046015601cec2a62c674dacc189e521b5
15x15 <65473527+15x15G@users.noreply.github.com>
2022-02-15 01:04:33 +0800

    Update 各种跳转.user.js

--- 各种跳转.user.js
+++ 各种跳转.user.js
@@ -1,18 +1,24 @@
 // ==UserScript==
 // @name         各种跳转
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @author       15x15
 // @match        *.scpwiki.com/*
-// @match        *.api.weibo.cn/*
+// @match        share.api.weibo.cn/*weibo_id
 // @grant        none
 // ==/UserScript==
 
 (function()
 {
     'use strict';
-    location.href = location.href.replace("www.scpwiki.com", "scp-wiki.wikidot.com")
-    location.href = location.href.replace(/https:\/\/share\.api\.weibo\.cn\/share\/\d+\.html\?weibo_id=/, "https://m.weibo.cn/detail/")
-
+   // location.href = location.href.replace("www.scpwiki.com", "scp-wiki.wikidot.com")
+   // location.href = location.href.replace(/https:\/\/share\.api\.weibo\.cn\/share\/\d+\.html\?weibo_id=/, "https://m.weibo.cn/detail/")
+   
+    if(location.href.match("scpwiki")){
+       location.href = location.href.replace("www.scpwiki.com", "scp-wiki.wikidot.com");
+    }
+    else if(location.href.match("api.weibo.cn")){
+        location.href = location.href.replace(/https:\/\/share\.api\.weibo\.cn\/share\/\d+\.html\?weibo_id=/, "https://m.weibo.cn/detail/")
+    }
     // Your code here...
 })();
