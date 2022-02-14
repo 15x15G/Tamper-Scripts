fbafa456ff6c822be3b38c8abf4f784036f32114
15x15 <65473527+15x15G@users.noreply.github.com>
2022-02-15 01:12:13 +0800

    Create prevent-pwa.user.js

---
+++ prevent-pwa.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         移除manifest以阻止pwa应用
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       15x15
+// @include         http://*
+// @include         https://*
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener("load", function() {
+  if (navigator.userAgent.indexOf('Mobile') === -1) {
+    document.querySelector('link[rel="manifest"]').remove();
+  }
+});
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+});
+
+/*
+https://stackoverflow.com/questions/57556321/prevent-pwa-install-prompt-chrome-76-on-desktop
+*/
