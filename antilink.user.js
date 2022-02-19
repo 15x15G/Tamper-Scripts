730beb4698cf23cb3df12010a6e5eb9d27a4f24f
15x15 <65473527+15x15G@users.noreply.github.com>
2022-02-19 08:26:09 +0800

    Update and rename tsdm-antilink.user.js to antilink.user.js

--- antilink.user.js
+++ antilink.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         tsdm关闭链接
+// @name         antilink关闭链接
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       15x15
 // @match        https://www.tsdm39.net/plugin.php?id=dsu_paulsign:sign
 // @match        https://www.tsdm39.net/plugin.php?id=np_cliworkdz:work
+// @match        *.stackoverflow.com/questions/*
 // @icon         https://www.google.com/s2/favicons?domain=tsdm39.net
 // @grant        none
 // ==/UserScript==
@@ -14,6 +15,7 @@
     'use strict';
     jQuery('.qdsmile a').each((i,item)=>{item.removeAttribute('href')})
     jQuery('#advids a').each((i,item)=>{item.removeAttribute('href')})
+    jQuery('.question-hyperlink').each((i,item)=>{item.removeAttribute('href')})
 
     // Your code here...
 })();
