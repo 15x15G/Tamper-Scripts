6d662bc27c85de0b75e288314499b3cb2fa611c0
15x15 <65473527+15x15G@users.noreply.github.com>
2022-05-19 10:35:20 +0800

    Update href-to-cn.user.js

--- href-to-cn.user.js
+++ href-to-cn.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         href-to-cn
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @author       15x15
 // @match        */en-us/*
 // @match        */ja-jp/*
+// @match        */ru-ru/*
+// @match        */zh-cn/*
 // @grant             GM_registerMenuCommand
 // ==/UserScript==
 
@@ -15,7 +17,10 @@
     function registerMenuCommand()
     {
         GM_registerMenuCommand('zh-cn', () =>{
-            window.location.href = window.location.href.replace(/\/ja-jp\/|\/en-us\//,'/zh-cn/')
+            window.location.href = window.location.href.replace(/\/ru-ru\/|\/ja-jp\/|\/en-us\//,'/zh-cn/')
+        });
+        GM_registerMenuCommand('en-us', () =>{
+            window.location.href = window.location.href.replace(/\/ru-ru\/|\/ja-jp\/|\/zh-cn\//,'/en-us/')
         });
     }
     registerMenuCommand();
