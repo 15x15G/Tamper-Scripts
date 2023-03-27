051909c5eb978405e3febacf5028ce2ca121a97f
15x15 <65473527+15x15G@users.noreply.github.com>
2023-03-27 10:16:10 +0800

    Update Discuzcheckin.user.js

--- Discuzcheckin.user.js
+++ Discuzcheckin.user.js
@@ -2,20 +2,24 @@
 // @name         Discuzcheckin
 // @name:zh-CN   Discuz签到
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.2
 // @description  try to take over the world!
 // @author       15x15
 // @match        https://gmgard.com/
 // @match        https://www.tsdm39.net/plugin.php?id=dsu_paulsign:sign
 // @match        http://www.txtnovel.pro/plugin.php*
-// @match        https://*.yamibo.com/*
+// @match        https://*.yamibo.com/
+// @match        https://*.hifini.com/*
 // @icon         https://icons.duckduckgo.com/ip2/tsdm39.net.ico
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    if(window.location.href.includes('yamibo') && document.querySelector('#nv_forum > div.oyheader > div > div > ul.header-tool.y > li > a:nth-child(3)')?.innerHTML.includes("签到")==true){
+    if(window.location.href.includes('hifini') && $("#sign").text()=="签到"){
+        $("#sg_sign").click();
+    }
+    else if(window.location.href.includes('yamibo') && document.querySelector('#nv_forum > div.oyheader > div > div > ul.header-tool.y > li > a:nth-child(3)')?.innerHTML.includes("签到")==true){
         var a = document.querySelector('#nv_forum > div.oyheader > div > div > ul.header-tool.y > li > a:nth-child(3)').href
         window.location.href=a
         return;
