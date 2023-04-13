20e8a292249514ca4850ad3063ebd2709da28fb9
15x15 <65473527+15x15G@users.noreply.github.com>
2023-04-13 21:19:24 +0800

    Create arrayGame.user.js

---
+++ arrayGame.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         arrayGame
+// @namespace    15x15
+// @version      0.1
+// @description  try to take over the world!
+// @author       15x15
+// @match        https://demonin.com/games/arrayGame/
+// @icon         https://icons.duckduckgo.com/ip2/demonin.com.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.addEventListener('keydown', function(event) {
+        switch (event.key) {
+            case "a":
+                buyUpgrade(1,4)
+                for(let i=5;i>=1;i--){
+                    buyMaxGenerators(1,i)
+                }
+                event.preventDefault();
+                break;
+            case "s":
+                save()
+                event.preventDefault();
+                // code for "up arrow" key press.
+                break;
+            case "b":
+                buyMaxABoosterators()
+                for(let i=5;i>=1;i--){
+                    buyMaxGenerators(2,i)
+                }
+                event.preventDefault();
+                // code for "left arrow" key press.
+                break;
+            case "q":
+                break;
+            default:
+                return; // Quit when this doesn't handle the key event.
+        }
+    });
+})();
